a326ac0c7cd9ec6129a7115d059003c3aae008d8
whhjdi <418605872@qq.com>
2018-06-12 12:28:49 +0800

    1. 优化代码结构,减少耦合,增加搜索结果条数判断,若没有更多数据则返回首页 2. 更改flex布局主轴的排列方式,让最后一列不满时,左对齐

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
 /*获取数据*/
 var getData = {
-    init: function (loading, keyword, index, navBottom, api) {
+    init: function (index, page) {
         this.isLoading = false
-        $itemContainer = $('.item-container')
-        this.start(loading, keyword, index, navBottom, api)
-    },
-    start: function (loading, keyword, index, navBottom, api) {
+        this.$search = $('.search')
+        this.$loading = $('.loading')
+        this.$welcome = $('.jumbotron')
+        this.$itemContainer = $('.item-container')
         if (this.isLoading) return
         this.isLoading = true
+        this.start(index, page)
+    },
+    start: function (index, page) {
         _this = this
-        console.log(api)
         $.ajax({
-            url: `https://api.douban.com/v2/${api}/search`,
+            url: `https://api.douban.com/v2/${_this.$itemContainer.attr('title')}/search`,
             type: 'GET',
             data: {
-                q: keyword,
+                q: _this.$search.val(),
                 start: index,
                 count: 20
             },
             dataType: 'jsonp'
         }).done(function (req) {
-            console.log(req)
-            setData.init(req)
-            loading.hide()
-            navBottom.show()
+            // console.log(req)
+            // console.log(index)
+            // console.log(page)
+            // console.log((index * page >= req.total))
+            if (index * page >= req.total || req.total === 0) {
+                alert('没有到更多搜索结果了,即将返回首页')
+                _this.$loading.hide()
+                _this.$welcome.show()
+            } else {
+                setData.init(req)
+                _this.$loading.hide()
+                _this.$navBottom.show()
+            }
         }).fail(function () {
-            console.log('error movie')
+            console.log('error,获取数据失败')
         }).always(function () {
             isLoading = false
         })
@@ -36,122 +47,30 @@ var getData = {
 var setData = {
     init: function (data) {
         this.$itemContainer = $('.item-container')
+        this.$loading = $('.loading')
+        this.$navBottom = $('.nav-bottom')
         this.start(data)
     },
     start: function (data) {
         _this = this
         if (_this.$itemContainer.attr('title') === 'movie') {
             data.subjects.forEach(function (movie) {
-                var $node = _this.createMovie(movie,data)
+                var $node = _this.createMovie(movie, data)
                 _this.$itemContainer.append($node)
             })
         } else if (_this.$itemContainer.attr('title') === 'music') {
             data.musics.forEach(function (music) {
-                var $node = _this.createMusic(music,data)
+                var $node = _this.createMusic(music, data)
                 _this.$itemContainer.append($node)
             })
         } else if (_this.$itemContainer.attr('title') === 'book') {
             data.books.forEach(function (book) {
-                var $node = _this.createBook(book,data)
+                var $node = _this.createBook(book, data)
                 _this.$itemContainer.append($node)
             })
         }
     },
-
-    createBook: function (book,data) {
-            var template = `<div class="item">
-    <a href="#" class='wrapper' target="_blank">
-    <div class="cover">
-        <img src="" alt="">
-    </div>
-    <div class="detail">
-        <h2></h2>
-        <div class="extra">评分:<span class="score"></span>分</div>
-        <div class="extra">作者: <span class="author"></span></div>
-        <div class="extra">出版时间: <span class="year"></span></div>
-        <div class="extra">出版社: <span class="publish"></span></div>
-        <div class="extra">定价: <span class="price"></span></div>
-    </div>
-    </a>
-</div>`
-
-            function getImages(_url) {
-                if (_url !== undefined) {
-                    let _u = _url.substring(7);
-                    return 'https://images.weserv.nl/?url=' + _u;
-                }
-            }
-
-            var $node = $(template)
-            $node.find('.total').text(data.total)
-            $node.find('.wrapper').attr('href', book.alt)
-            $node.find('.cover img').attr('src', getImages(book.image))
-            $node.find('.detail h2').text(book.title)
-            $node.find('.score').text(book.rating.average)
-            $node.find('.year').text(book.pubdate)
-            $node.find('.publish').text(book.publisher)
-            $node.find('.price').text(book.price)
-            $node.find('.author').text(function () {
-                if (book.author) {
-                    return book.author.join(' ')
-                } else {
-                    return '暂无'
-                }
-            })
-            return $node
-    },
-    createMusic: function (music,data) {
-            var template = `<div class="item">
-      <a href="#" class='wrapper' target="_blank">
-        <div class="cover">
-          <img src="" alt="">
-        </div>
-        <div class="detail">
-          <h2></h2>
-          <div class="extra">评分:<span class="score"></span>分</div>
-          <div class="extra">发行时间: <span class="year"></span></div>
-          <div class="extra">出版: <span class="publish"></span></div>
-          <div class="extra">歌手: <span class="singer"></span></div>
-        </div>
-      </a>
-    </div>`
-
-            function getImages(_url) {
-                if (_url !== undefined) {
-                    let _u = _url.substring(7);
-                    return 'https://images.weserv.nl/?url=' + _u;
-                }
-            }
-            var $node = $(template)
-            $node.find('.total').text(data.total)
-            $node.find('.wrapper').attr('href', music.alt)
-            $node.find('.cover img').attr('src', getImages(music.image))
-            $node.find('.detail h2').text(music.title)
-            $node.find('.score').text(music.rating.average)
-            $node.find('.year').text(function () {
-                if (music.attrs.pubdate) {
-                    return music.attrs.pubdate.join(' ')
-                } else {
-                    return '暂无'
-                }
-            })
-            $node.find('.publish').text(function () {
-                if (music.attrs.publisher) {
-                    return music.attrs.publisher.join(' ')
-                } else {
-                    return '暂无'
-                }
-            })
-            $node.find('.singer').text(function () {
-                if (music.attrs.singer) {
-                    return music.attrs.singer.join(' ')
-                } else {
-                    return '暂无'
-                }
-            })
-            return $node
-    },
-    createMovie: function (movie,data) {
+    createMovie: function (movie, data) {
         var template = `<div class="item">
   <a href="#" class='wrapper' target="_blank">
     <div class="cover">
@@ -188,17 +107,114 @@ var setData = {
             movie.directors.forEach(function (item) {
                 directorsArr.push(item.name)
             })
-            return directorsArr.join('、')
+            directorsArr.length = 3
+            return directorsArr.join(' ')
         })
         $node.find('.actor').text(function () {
             var actorArr = []
             movie.casts.forEach(function (item) {
                 actorArr.push(item.name)
             })
-            return actorArr.join('、')
+            actorArr.length = 3
+            return actorArr.join(' ')
         })
         return $node
-    }
+    },
+    createMusic: function (music, data) {
+        var template = `<div class="item">
+  <a href="#" class='wrapper' target="_blank">
+    <div class="cover">
+      <img src="" alt="">
+    </div>
+    <div class="detail">
+      <h2></h2>
+      <div class="extra">评分:<span class="score"></span>分</div>
+      <div class="extra">发行时间: <span class="year"></span></div>
+      <div class="extra">出版: <span class="publish"></span></div>
+      <div class="extra">歌手: <span class="singer"></span></div>
+    </div>
+  </a>
+</div>`
+
+        function getImages(_url) {
+            if (_url !== undefined) {
+                let _u = _url.substring(7);
+                return 'https://images.weserv.nl/?url=' + _u;
+            }
+        }
+        var $node = $(template)
+        $node.find('.total').text(data.total)
+        $node.find('.wrapper').attr('href', music.alt)
+        $node.find('.cover img').attr('src', getImages(music.image))
+        $node.find('.detail h2').text(music.title)
+        $node.find('.score').text(music.rating.average)
+        $node.find('.year').text(function () {
+            if (music.attrs.pubdate) {
+                return music.attrs.pubdate.join(' ')
+            } else {
+                return '暂无'
+            }
+        })
+        $node.find('.publish').text(function () {
+            if (music.attrs.publisher) {
+                return music.attrs.publisher.join(' ')
+            } else {
+                return '暂无'
+            }
+        })
+        $node.find('.singer').text(function () {
+            if (music.attrs.singer) {
+                return music.attrs.singer.join(' ')
+            } else {
+                return '暂无'
+            }
+        })
+        return $node
+    },
+    createBook: function (book, data) {
+        var template = `<div class="item">
+    <a href="#" class='wrapper' target="_blank">
+    <div class="cover">
+        <img src="" alt="">
+    </div>
+    <div class="detail">
+        <h2></h2>
+        <div class="extra">评分:<span class="score"></span>分</div>
+        <div class="extra">作者: <span class="author"></span></div>
+        <div class="extra">出版时间: <span class="year"></span></div>
+        <div class="extra">出版社: <span class="publish"></span></div>
+        <div class="extra">定价: <span class="price"></span></div>
+    </div>
+    </a>
+</div>`
+
+        function getImages(_url) {
+            if (_url !== undefined) {
+                let _u = _url.substring(7);
+                return 'https://images.weserv.nl/?url=' + _u;
+            }
+        }
+
+        var $node = $(template)
+        $node.find('.total').text(data.total)
+        $node.find('.wrapper').attr('href', book.alt)
+        $node.find('.cover img').attr('src', getImages(book.image))
+        $node.find('.detail h2').text(book.title)
+        $node.find('.score').text(book.rating.average)
+        $node.find('.year').text(book.pubdate)
+        $node.find('.publish').text(book.publisher)
+        $node.find('.price').text(book.price)
+        $node.find('.author').text(function () {
+            if (book.author) {
+                return book.author.join(' ')
+            } else {
+                return '暂无'
+            }
+        })
+        return $node
+    },
+
+
 }
 
 /*绑定事件*/
@@ -210,52 +226,58 @@ var bindEvent = {
         this.$book = $('.book')
         this.$welcome = $('.jumbotron')
         this.$previous = $('.previous')
+        this.$next = $('.next')
         this.$siteContent = $('.site-content')
         this.$navBottom = $('.nav-bottom')
-        this.$next = $('.next')
         this.$itemContainer = $('.item-container')
-        this.$loading = $('.loading')
         this.index = 0
-        this.$music = $('.music')
-        this.$itemContainer = $('.item-container')
         this.$loading = $('.loading')
         this.$search = $('.search')
+        this.page = 0
         this.bind()
     },
     bind: function () {
         var _this = this
-        
+        this.$search.keydown(function (e) {
+            if (e.keyCode == "13") {
+                _this.$keyword = $('.search').val()
+                if (_this.$keyword) {
+                    window.open("//" + "www.baidu.com/s?wd=" + _this.$keyword)
+                    return false
+                }
+            }
+        })
         this.$movie.on('click', function () {
             _this.$itemContainer.attr('title', 'movie')
             _this.$loading.show()
             _this.$itemContainer.empty()
-            _this.$keyword = $('.search').val()
             _this.index = 0
+            _this.page = 0
             _this.$document.scrollTop(0)
             _this.$navBottom.hide()
-            getData.init(_this.$loading, _this.$keyword, _this.index, _this.$navBottom, _this.$itemContainer.attr('title'))
+            getData.init(_this.index)
             _this.$welcome.hide()
         })
         this.$music.on('click', function () {
             _this.$itemContainer.attr('title', 'music')
             _this.$loading.show()
             _this.$itemContainer.empty()
-            this.$keyword = _this.$search.val()
             _this.index = 0
+            _this.page = 0
             _this.$document.scrollTop(0)
             _this.$navBottom.hide()
-            getData.init(_this.$loading, this.$keyword, _this.index, _this.$navBottom, _this.$itemContainer.attr('title'))
+            getData.init(_this.index)
             _this.$welcome.hide()
         })
         this.$book.on('click', function () {
             _this.$itemContainer.attr('title', 'book')
             _this.$loading.show()
             _this.$itemContainer.empty()
-            this.$keyword = _this.$search.val()
             _this.index = 0
+            _this.page = 0
             _this.$document.scrollTop(0)
             _this.$navBottom.hide()
-            getData.init(_this.$loading, this.$keyword, _this.index, _this.$navBottom, _this.$itemContainer.attr('title'))
+            getData.init(_this.index)
             _this.$welcome.hide()
         })
         this.$previous.on('click', function () {
@@ -266,9 +288,10 @@ var bindEvent = {
                 _this.$itemContainer.empty()
                 _this.$navBottom.hide()
                 _this.index -= 20
-                this.$keyword = _this.$search.val()
                 _this.$document.scrollTop(0)
-                getData.init(_this.$loading, this.$keyword, _this.index, _this.$navBottom, _this.$itemContainer.attr('title'))
+                _this.page--
+                getData.init(_this.index, _this.page)
+
             }
         })
         this.$next.on('click', function () {
@@ -276,13 +299,14 @@ var bindEvent = {
             _this.$itemContainer.empty()
             _this.$navBottom.hide()
             _this.index += 20
-            this.$keyword = _this.$search.val()
             _this.$document.scrollTop(0)
-            getData.init(_this.$loading, this.$keyword, _this.index, _this.$navBottom, _this.$itemContainer.attr('title'))
+            _this.page++
+            getData.init(_this.index, _this.page)
         })
     }
 }
-//入口
+/*入口,不写也行,或许直接 
+bindEvent.init()更好 */
 var app = {
     init: function () {
         bindEvent.init()
